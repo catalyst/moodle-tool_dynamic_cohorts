fa28578c6ee29cd7dc331c119364268d4efe818d
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2024-03-21 21:52:02 +1100

    Fix an issue with rule processing by cron

--- classes/rule_manager.php
+++ classes/rule_manager.php
@@ -24,6 +24,10 @@ use tool_dynamic_cohorts\event\rule_created;
 use tool_dynamic_cohorts\event\rule_deleted;
 use tool_dynamic_cohorts\event\rule_updated;
 
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot.'/cohort/lib.php');
+
 /**
  * Rule manager class.
  *
